dc0e616ef407040d60c4d7dceb02a9d5923e7a24
Jarek <yaraslau.s@gmail.com>
2023-11-22 16:11:16 +0100

    style: recording page & single audio => list of audios

--- src/pages/RecordPage.vue
+++ src/pages/RecordPage.vue
@@ -1,54 +1,63 @@
 <template>
-  <q-page class="flex column flex-center">
-    <div class="q-pa-md q-gutter-sm">
+  <q-page class="flex column items-center q-gutter-sm">
+    <div class="q-pa-xl q-mt-xl">
       <q-btn @click="startRecording" :disabled="recording" round color="negative" icon="mic" :class="{
         'animated-pulse': !recording
-      }" />
-      <q-btn @click="stopRecording" :disabled="!recording" round color="negative" icon="stop" />
+      }" size="xl" />
     </div>
-
-    <audio ref="audioPlayer" controls></audio>
+    <audio v-for="(audio) in audios" :key="audio" :src="audio" controls></audio>
+    <q-page-sticky v-if="recording" position="bottom-right" :offset="[15, 18]" style="z-index: 10000">
+      <q-btn fab icon="stop" color="negative" @click="stopRecording" />
+    </q-page-sticky>
   </q-page>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { useQuasar, QSpinnerAudio } from 'quasar'
 
 const recording = ref(false)
 const mediaRecorder = ref(null)
 const audioChunks = ref([])
+const audios = ref([])
 
-const audioPlayer = ref(null);
+const $q = useQuasar()
 
 const startRecording = async () => {
   try {
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    $q.loading.show({
+      spinner: QSpinnerAudio,
+      backgroundColor: 'primary'
+    })
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
 
-    mediaRecorder.value = new MediaRecorder(stream);
+    mediaRecorder.value = new MediaRecorder(stream)
 
     mediaRecorder.value.ondataavailable = (event) => {
       if (event.data.size > 0) {
-        audioChunks.value.push(event.data);
+        audioChunks.value.push(event.data)
       }
-    };
+    }
 
     mediaRecorder.value.onstop = () => {
-      const audioBlob = new Blob(audioChunks.value, { type: 'audio/wav' });
-      const audioUrl = URL.createObjectURL(audioBlob);
-      audioPlayer.value.src = audioUrl;
-    };
+      const audioBlob = new Blob(audioChunks.value, { type: 'audio/wav' }) //{ type: 'audio/ogg codecs=opus' }
+      const audioUrl = URL.createObjectURL(audioBlob)
+      audioChunks.value = []
+      audios.value.push(audioUrl)
+    }
 
-    mediaRecorder.value.start();
-    recording.value = true;
+    mediaRecorder.value.start()
+    recording.value = true
   } catch (error) {
-    console.error('Error accessing microphone:', error);
+    console.error('Error accessing microphone:', error)
   }
 }
 
 const stopRecording = () => {
   if (mediaRecorder.value) {
-    mediaRecorder.value.stop();
-    recording.value = false;
+    mediaRecorder.value.stop()
+    recording.value = false
+    $q.loading.hide()
   }
 }
 </script>
