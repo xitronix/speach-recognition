097ba3d6b350024f718171fb137986bc48b105c4
Jarek <yaraslau.s@gmail.com>
2023-11-22 13:56:57 +0100

    feat: record functionality

--- src/pages/RecordPage.vue
+++ src/pages/RecordPage.vue
@@ -1,17 +1,75 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex column flex-center">
+    <div class="q-pa-md q-gutter-sm">
+      <q-btn @click="startRecording" :disabled="recording" round color="negative" icon="mic" :class="{
+        'animated-pulse': !recording
+      }" />
+      <q-btn @click="stopRecording" :disabled="!recording" round color="negative" icon="stop" />
+    </div>
+
+    <audio ref="audioPlayer" controls></audio>
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
+import { ref } from 'vue'
 
-export default defineComponent({
-  name: 'IndexPage'
-})
+const recording = ref(false)
+const mediaRecorder = ref(null)
+const audioChunks = ref([])
+
+const audioPlayer = ref(null);
+
+const startRecording = async () => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+    mediaRecorder.value = new MediaRecorder(stream);
+
+    mediaRecorder.value.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        audioChunks.value.push(event.data);
+      }
+    };
+
+    mediaRecorder.value.onstop = () => {
+      const audioBlob = new Blob(audioChunks.value, { type: 'audio/wav' });
+      const audioUrl = URL.createObjectURL(audioBlob);
+      audioPlayer.value.src = audioUrl;
+    };
+
+    mediaRecorder.value.start();
+    recording.value = true;
+  } catch (error) {
+    console.error('Error accessing microphone:', error);
+  }
+}
+
+const stopRecording = () => {
+  if (mediaRecorder.value) {
+    mediaRecorder.value.stop();
+    recording.value = false;
+  }
+}
 </script>
+
+<style>
+.animated-pulse {
+  animation: pulse 2s linear infinite;
+  transform-origin: center center;
+}
+
+@keyframes pulse {
+  0% {
+    transform: scale(0.95);
+  }
+
+  70% {
+    transform: scale(1.1);
+  }
+
+  100% {
+    transform: scale(0.95);
+  }
+}
+</style>
